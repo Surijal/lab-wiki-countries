95602ebe137bdb1ee4ca89ea9d43e7d22e025294
Boris Vogt <boris.vogt@posteo.de>
2019-12-05 23:49:19 +0100

    local server add

--- starter-code/src/Server.js
+++ starter-code/src/Server.js
@@ -7,16 +7,16 @@ const cors = require('cors')
 const app = express();
 app.use(cors())
 
-app.get('/:country', (req, res, next) => {
-    var countries = require("./countries.json")
+// app.get('/:country', (req, res, next) => {
+//     var countries = require("./countries.json")
     
-    var searchCountry = req.params.country.toLowerCase()
-    var countriesFiltered = countries.filter((country)=> 
-    country.name.common.toLowerCase().includes(searchCountry) || country.name.official.includes(searchCountry)
-    )
+//     var searchCountry = req.params.country.toLowerCase()
+//     var countriesFiltered = countries.filter((country)=> 
+//     country.name.common.toLowerCase().includes(searchCountry) || country.name.official.includes(searchCountry)
+//     )
     
-    res.status(200).json(countriesFiltered);
-});
+//     res.status(200).json(countriesFiltered);
+// });
 
 app.get('/', (req, res, next) => {
     var countries = require('./countries.json')
